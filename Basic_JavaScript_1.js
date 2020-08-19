a69e42df14ff23bda372289b8640bc476b1c94c5
Ryan Bunker <r3bunker@gmail.com>
2020-08-19 08:24:43 -0600

    Adding second html assingment files

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Is this thing on?");
+
+function My_First_Function() {
+	var String = "Kiss me, I'm Italian!";
+	var result = String.fontcolor("red");
+	document.getElementById("Italian").innerHTML =
+	result;
+}
